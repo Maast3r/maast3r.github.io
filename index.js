8e7d96ebc93f5367501151f2a0218512700b177c
Andrew Ma <maastergotmail@gmail.com>
2016-05-28 22:58:44 -0400

    started on desktop

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
 
   //
+  // FOLDERS
+  //
+  $("#resume").dblclick(function() {
+    var wind = window.open("Resume.pdf", "_blank");
+    wind.focus();
+  });
+
+
+  $("#rose").dblclick(function() {
+    var wind = window.open("http://www.rose-hulman.edu/news/academics/2015/rose-hulman-ranked-nations-top-undergraduate-engineering-college-for-17th-straight-year-in-us-news-world-reports-annual-college-guide.aspx", "_blank");
+    wind.focus();
+  });
+
+  //
   // CLOCK
   //
   function getMinutes() {
@@ -25,22 +39,22 @@ $(document).ready(function(){
   //
   // TASKBAR BUTTONS
   //
-  $("#github").click(function(){
+  $("#github").click(function() {
     var wind = window.open("https://github.com/Maast3r", "_blank");
     wind.focus();
   });
 
-  $("#linkedin").click(function(){
+  $("#linkedin").click(function() {
     var wind = window.open("https://www.linkedin.com/in/softwarelover", "_blank");
     wind.focus();
   });
 
-  $("#spotify").click(function(){
+  $("#spotify").click(function() {
     var wind = window.open("https://play.spotify.com/user/1230773325", "_blank");
     wind.focus();
   });
 
-  $("#steam").click(function(){
+  $("#steam").click(function() {
     var wind = window.open("http://steamcommunity.com/id/maasterma", "_blank");
     wind.focus();
   });
