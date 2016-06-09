ab0c0d1ee30517a18f15c80e8f24850ad8e7a553
Andrew Ma <maastergotmail@gmail.com>
2016-06-09 00:22:40 -0400

    finished pong intergration. working on adding more to the terminal

--- js/terminal.js
+++ js/terminal.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
-
-  $("#terminal-close").click(function() {
-    $("#terminal").hide();
-  });
-
+    $("#terminal-close").click(function() {
+        $("#terminal").hide();
+    });
 });
+
+function helpHandler() {
+    var addedLine = "<div class='terminal-line-history'>pong        Starts a game of Pong</div>";
+    $("#terminal-history").append(addedLine);
+    addedLine =  "<div class='terminal-line-history'>help        Displays all commands</div>";
+    $("#terminal-history").append(addedLine);
+}
+
+function defaultHandler() {
+    var addedLine = "<div class='terminal-line-history'>Sorry, the command: "
+        + command + " is not supported</div>";
+    $("#terminal-history").append(addedLine);
+}
